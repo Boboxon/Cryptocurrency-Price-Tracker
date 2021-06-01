4d9838e28723fc48396dc6eb6c9856644a20b97f
Rajabov Bobokhon <rboboxon6055@gmail.com>
2021-06-02 00:02:43 +0900

    fetch the data from api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  // const [darkMode, setDarkMode] = useState(false);
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">Search a currency</h1>
+        <h1 className="coin-text">Search a Cryptocurrency</h1>
         <form>
           <input
             className="coin-input"
             type="text"
-            // onChange={handleChange}
+            onChange={handleChange}
             placeholder="Search"
           />
         </form>
       </div>
+      <div className="nameContainer">
+        <div className="currencyName">
+          <h3>Name</h3>
+        </div>
+        <div className="currencyPrice">
+          <h3>Price</h3>
+        </div>
+        <div className="currencyMarket">
+          <h3>Market Cap</h3>
+        </div>
+        <div className="currencyPercent">
+          {" "}
+          <h3>24h %</h3>
+        </div>
+        <div className="currencyValume">
+          <h3>Valume</h3>
+        </div>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </div>
   );
 }
